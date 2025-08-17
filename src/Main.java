4374c959314d3a24fab2a8719eb84a0e03962d75
CaioTBarbieri <caiotbbarbieri@gmail,com>
2025-08-17 17:34:19 -0300

    Add .gitignore and implement binary tree methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+
+
+        arvore.inserir(69);
+        arvore.inserir(37);
+        arvore.inserir(60);
+
+        arvore.imprime_preFixado();
+
+        arvore.inserir(50);
+        arvore.imprime_preFixado();
+
+        arvore.pesquisa(69);
+        System.out.println(arvore.pesquisa(69));
 
 
 
